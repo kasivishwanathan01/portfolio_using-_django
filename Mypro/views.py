8aedd1dd49aa8738a3dbb80a4f356ff8ab6edd9d
kasivishwanathan <140682338+kasivishwanathan01@users.noreply.github.com>
2023-08-03 11:56:27 +0530

    Add files via upload

---
+++ Mypro/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def blog_single(request):
+    return render(request, 'blog_single.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def index(request):
+    return render(request, 'index.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
